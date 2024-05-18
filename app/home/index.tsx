a3659188b7162ffa46d961e2b08c5a77beb59cc7
Vishal Kumar <142484106+VKumar786@users.noreply.github.com>
2024-05-18 10:32:19 +0530

    Update index.tsx

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,38 +1,43 @@
-import { Stack } from "expo-router";
-import { Platform, View } from "react-native";
-import { WebView } from "react-native-webview";
+import { Text, Platform, SafeAreaView, StyleSheet } from 'react-native';
 
-export default function HomeScreen() {
+// You can import supported modules from npm
+import { Card } from 'react-native-paper';
+
+// or any files within the Snack
+import AssetExample from './components/AssetExample';
+import { WebView } from 'react-native-webview';
+
+export default function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <Stack.Screen
-        options={{
-          title: "My home",
-          headerStyle: { backgroundColor: "#f4511e" },
-          headerTintColor: "#fff",
-          headerTitleStyle: {
-            fontWeight: "bold",
-          },
-
-          headerTitle: (props) => null,
-        }}
-      />
-      {Platform.OS === "web" ? (
+    <SafeAreaView style={styles.container}>
+      {Platform.OS === 'web' ? (
         <iframe
           width="100%"
           height="100vh"
           style={{ flex: 1, padding: 0, border: 0 }}
           src="http://diabetes-prediction-ccvi.onrender.com/"
-          allow="autoplay; encrypted-media"
-        ></iframe>
+          allow="autoplay; encrypted-media"></iframe>
       ) : (
         <WebView
-          source={{
-            html: '<iframe width="100%" height="100vh" style="flex: 1; padding: 0; border: 0;" src="http://diabetes-prediction-ccvi.onrender.com/" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>',
+          source={{ uri: 'http://diabetes-prediction-ccvi.onrender.com/' }}
+          style={{
+            flex: 1,
+            backgroundColor: 'red',
+            marginTop: 20,
           }}
-          style={{ marginTop: 20 }}
+          allowsInlineMediaPlayback
+          mediaPlaybackRequiresUserAction={false}
         />
       )}
-    </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    marginTop: 25,
+  },
+});
